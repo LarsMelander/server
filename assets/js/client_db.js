71b3d979b6fb8a48fcb874c0f9cdbcdacfce870e
Augustin82 <augustin.ragon@gmail.com>
2021-04-01 20:46:21 +0200

    Compiles

--- assets/js/client_db.js
+++ assets/js/client_db.js
@@ -1,13 +1,17 @@
-import clientDb from "./routes_db"
+import { Elm } from "../elm-db/src/Main.elm";
+import clientDb from "./routes_db";
 
-import socket from "./socket"
+import socket from "./socket";
 
-let ledgers_channel = socket.channel("ledgers", {})
+let ledgers_channel = socket.channel("ledgers", {});
 
-ledgers_channel.join()
-    .receive("ok", resp => {
-        console.log("Joined successfully", resp)
-        clientDb(ledgers_channel)
-    })
-    .receive("error", resp => { console.log("Unable to join", resp) })
-    .receive("timeout", () => console.log("Networking issue. Still waiting..."))
+ledgers_channel
+  .join()
+  .receive("ok", (resp) => {
+    console.log("Joined successfully", resp);
+    clientDb(ledgers_channel);
+  })
+  .receive("error", (resp) => {
+    console.log("Unable to join", resp);
+  })
+  .receive("timeout", () => console.log("Networking issue. Still waiting..."));
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,69 +1,80 @@
-const path = require('path');
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const HardSourceWebpackPlugin = require("hard-source-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = (env, options) => {
-  const devMode = options.mode !== 'production';
+  const devMode = options.mode !== "production";
 
   return {
     optimization: {
       minimizer: [
         new TerserPlugin({ cache: true, parallel: true, sourceMap: devMode }),
         new OptimizeCSSAssetsPlugin({}),
-      ]
+      ],
     },
     entry: {
-      'client-db': './js/client_db.js',
-      'client-ledger': './js/client_ledger.js',
+      "client-db": "./js/client_db.js",
+      "client-ledger": "./js/client_ledger.js",
     },
     output: {
-      filename: '[name].js',
-      path: path.resolve(__dirname, '../priv/static/js'),
-      publicPath: '/js/',
+      filename: "[name].js",
+      path: path.resolve(__dirname, "../priv/static/js"),
+      publicPath: "/js/",
     },
-    devtool: devMode ? 'eval-cheap-module-source-map' : undefined,
-    // devtool: devMode ? 'inline-source-map' : undefined,
+    devtool: devMode ? "eval-cheap-module-source-map" : undefined,
     module: {
       rules: [
+        {
+          test: /\.elm$/,
+          include: /elm-db/,
+          exclude: [/elm-stuff/, /node_modules/],
+          use: [
+            {
+              loader: "elm-hot-webpack-loader",
+            },
+            {
+              loader: "elm-webpack-loader",
+              options: {
+                debug: options.mode === "development",
+                optimize: options.mode === "production",
+                cwd: path.resolve(__dirname + "/elm-db/"),
+              },
+            },
+          ],
+        },
+        {
+          test: /\.elm$/,
+          include: /elm-ledger/,
+          exclude: [/elm-stuff/, /node_modules/],
+          use: [
+            {
+              loader: "elm-hot-webpack-loader",
+            },
+            {
+              loader: "elm-webpack-loader",
+              options: {
+                debug: options.mode === "development",
+                optimize: options.mode === "production",
+                cwd: path.resolve(__dirname + "/elm-ledger/"),
+              },
+            },
+          ],
+        },
         {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
-          }
-        },
-        {
-          test: /\.elm$/,
-          exclude: [/elm-stuff/, /node_modules/],
-          use: [
-            {
-              loader: 'elm-hot-webpack-loader',
-            }, {
-              loader: 'elm-webpack-loader',
-              // options: {
-                // cwd: path.resolve(__dirname, 'elm'),
-                // files: [
-                //   'elm/src/Db/Main.elm',
-                //   'elm/src/Ledger/Main.elm',
-                // ].map(name => path.resolve(__dirname, name)),
-              // }
-            }
-          ],
-
-          options: {
-            debug: options.mode === "development",
-            optimize: options.mode === "production",
+            loader: "babel-loader",
           },
         },
-      ]
+      ],
     },
     plugins: [
-      new MiniCssExtractPlugin({ filename: '../css/[name].css' }),
-      new CopyWebpackPlugin([{ from: 'static/', to: '../' }]),
-    ]
-      .concat(devMode ? [new HardSourceWebpackPlugin()] : []),
-  }
+      new MiniCssExtractPlugin({ filename: "../css/[name].css" }),
+      new CopyWebpackPlugin([{ from: "static/", to: "../" }]),
+    ].concat(devMode ? [new HardSourceWebpackPlugin()] : []),
+  };
 };
